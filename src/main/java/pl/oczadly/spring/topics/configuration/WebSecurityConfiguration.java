80ed5683204342d0b1c16e35b2abba40633e1c99
Pawlllosss <oczadlypawe@gmail.com>
2019-10-03 11:04:31 +0200

    # modified web security setting # renamed CorsConfiguration class

--- src/main/java/pl/oczadly/spring/topics/configuration/WebSecurityConfiguration.java
+++ src/main/java/pl/oczadly/spring/topics/configuration/WebSecurityConfiguration.java
@@ -14,9 +14,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import pl.oczadly.spring.topics.user.authentication.control.UserAuthenticationService;
 import pl.oczadly.spring.topics.user.authentication.control.token.JWTAuthenticationFilter;
 import pl.oczadly.spring.topics.user.authentication.control.token.JWTAuthenticationUnauthorizedResponseEntryPoint;
+
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 
 @Configuration
@@ -43,6 +51,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors()
+                .configurationSource(getCorsConfigurationSource())
                 .and()
                 .csrf()
                     .disable()
@@ -73,6 +82,18 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authenticated();
 
         http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    private CorsConfigurationSource getCorsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        List<String> allowOrigins = Collections.singletonList("*");
+        configuration.setAllowedOrigins(allowOrigins);
+        configuration.setAllowedMethods(singletonList("*"));
+        configuration.setAllowedHeaders(singletonList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
