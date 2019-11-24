b0613dbdd06fa015946c97c769a3da52d7c94d7d
Pawlllosss <oczadlypawe@gmail.com>
2019-11-24 21:46:01 +0100

    + added links to votes for course

--- src/main/java/pl/oczadly/spring/topics/course/boundary/CourseResourceAssembler.java
+++ src/main/java/pl/oczadly/spring/topics/course/boundary/CourseResourceAssembler.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 import pl.oczadly.spring.topics.course.entity.Course;
 import pl.oczadly.spring.topics.topic.boundary.TopicRestController;
 import pl.oczadly.spring.topics.topic.entity.TopicDTO;
+import pl.oczadly.spring.topics.user.authentication.entity.UserAuthenticationDetails;
+import pl.oczadly.spring.topics.voting.boundary.VoteRestController;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -28,7 +30,9 @@ public class CourseResourceAssembler implements ResourceAssembler<Course, Resour
                 .withRel("topics");
         Link createTopicLink = linkTo(methodOn(TopicRestController.class).createTopic(new TopicDTO(), courseId))
                 .withRel("createTopic");
+        Link userVotesLink = linkTo(methodOn(VoteRestController.class).getUserVotesForTheCourse(UserAuthenticationDetails.builder().build(), courseId))
+                .withRel("userVotes");
 
-        return new Resource<>(course, selfLink, updateLink, deleteLink, topicsLink, createTopicLink);
+        return new Resource<>(course, selfLink, updateLink, deleteLink, topicsLink, createTopicLink, userVotesLink);
     }
 }
